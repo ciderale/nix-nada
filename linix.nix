92497e6e037a60bb62959b3852e4b8fb073a20c0
Alain Lehmann <alain.lehmann@gmail.com>
2018-05-21 23:15:44 +0200

    linix: update to ubuntu 18.04
    - adding netbase for /etc/protocols
    - locales for en_US.UTF-8 generation

--- linix.nix
+++ linix.nix
@@ -1,15 +1,15 @@
 with (import <nixpkgs> {});
 
 let dockerfile = writeText "Dockerfile" ''
-  From ubuntu
+  From ubuntu:18.04
 
   # boot strapping environment
   RUN apt-get update
-  RUN apt-get install -y bzip2 curl man
+  RUN apt-get install -y bzip2 curl man netbase locales
 
   # setup locale
-  RUN sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && \
-      locale-gen
+  RUN sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen \
+      && locale-gen
   ENV LANG en_US.UTF-8  
   ENV LANGUAGE en_US:en  
   ENV LC_ALL en_US.UTF-8     
