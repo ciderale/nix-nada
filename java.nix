63dd4e21160884360e3abcb3f9a271689d7cb33d
Alain Lehmann <alain.lehmann@gmail.com>
2018-11-11 20:42:33 +0100

    add overlay for selecting java version

---
+++ java.nix
@@ -0,0 +1,21 @@
+{
+  custom = self: super: rec {
+
+    jdk11 = super.jdk10.overrideAttrs (oldAttrs: rec {
+      version = "11.2.3";
+      openjdk = "11.0.1";
+      name = "zulu-${version}";
+
+      platform = "macosx";
+      extension = "zip";
+
+      src = super.fetchurl {
+        url = "https://cdn.azul.com/zulu/bin/zulu${version}-jdk${openjdk}-${platform}_x64.${extension}";
+        sha256 = "0zc311smgz6zsy41qiimrywlbfn8451j13g32dlms67k4j5slyqv";
+      };
+    });
+
+  };
+
+  select = jdkversion: self: super: { jdk = self."${jdkversion}"; };
+}
