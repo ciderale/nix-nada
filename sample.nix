989c5b5b815f7f5f0265891c7430b6685f347769
Alain Lehmann <alain.lehmann@gmail.com>
2018-11-12 00:23:36 +0100

    experiment with the sample.nix

--- sample.nix
+++ sample.nix
@@ -1,28 +1,58 @@
-with import <nixpkgs> {};
+{ system ? builtins.currentSystem
+}:
+
+let
+  selectJS = self: super: { nodejs = self.nodejs-10_x; } ;
+in
+
+with import <nixpkgs> {
+  inherit system;
+  overlays = [ selectJS ];
+};
+
+let
+
+  # for some very simple one-off script files (make it executable)
+  helloworld = writeShellScriptBin "helloworld" ''
+  	echo "Hello World $1"
+  '';
+
+in 
 
 stdenv.mkDerivation rec {
   name = "my-project";
   version = "0.1";
+  
+  buildInputs = [ git nodejs ];
 
-  buildInputs = [ git kpcli gradle_3_5 nodejs-8_x ansible openssh vim ];
+  # Run only the installPhase -- no unpacking -- phases must be "quoted"
+  phases = [ "installPhase" ];
+  installPhase = ''
+    mkdir -p $out/bin;
+
+    # output a bunch of versions
+    echo "Built on: $(date)" >> $out/versions;
+    uname -a >> $out/versions;
+    node --version >> $out/versions;
+    # gradle -version >> $out/versions;
+
+    # 1) Just link the helloworld binary
+    # ln -s ${helloworld}/bin/helloworld $out/bin
+
+    # 2) OR create a wrapper with a version preamble
+    cat > $out/bin/helloworld << EOF
+    #!${bash}/bin/bash
+    ${coreutils}/bin/cat $out/versions
+    exec ${helloworld}/bin/helloworld \$*
+    EOF
+    chmod +x $out/bin/helloworld
+  '';
 
   shellHook = ''
     export PS1="\\u@\h \\W]\\$ ${name}> "
+    echo "Your PATH variable contains:"
     echo $PATH | tr : '\n'
-  '';
-
-  # Actually building it requires also
-
-  builder = builtins.toFile "builder.sh" ''
-    source $stdenv/setup;
-    mkdir -p $out;
-    ln -s $theprogram/bin $out/bin
-    gradle -version >> $out/versions;
-    node --version >> $out/versions;
-  '';
-
-  theprogram = writeShellScriptBin "helloworld" ''
-  	echo "Hello World $1"
+    echo "  that was your path variable"
   '';
 
 }
