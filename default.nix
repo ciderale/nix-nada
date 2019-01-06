cc67efb826c26b91464220f9260f1538e077a526
Alain Lehmann <alain.lehmann@gmail.com>
2019-01-06 22:07:53 +0100

    improve vim configuration

--- default.nix
+++ default.nix
@@ -1,16 +1,20 @@
 with (import ./nix/1809-darwin.nix {});
 
 let
+  git = gitFull;
   nix-pinning = callPackage ./packages/nix-pinning.nix {};
   zsh = callPackage ./packages/zsh.nix {};
-  vim = callPackage ./packages/vim {};
+  vim = callPackage ./packages/vim { git = gitFull; };
 in
 
 # install globally with
 # nix-env -f ./default.nix -iA buildInputs
 mkShell {
   buildInputs = with import ./default.nix; [
-    nix-pinning zsh vim which coreutils
+    nix-pinning
+    zsh vim git
+    which coreutils less
+    curl jq
   ];
   shellHook = ''
   '';
