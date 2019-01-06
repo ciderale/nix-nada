19831ba6640225940fb58430d24e8ad40f8ccb6e
Alain Lehmann <alain.lehmann@gmail.com>
2019-01-06 21:37:13 +0100

    adding script for pinning version

---
+++ packages/nix-pinning.nix
@@ -0,0 +1,28 @@
+{ writeShellScriptBin, nix, git, coreutils }:
+
+writeShellScriptBin "nix-pinning" ''
+  set -e
+  BRANCH=''${1:-nixos-unstable}
+  DATE=$(${coreutils}/bin/date +%Y-%m-%d)
+  if [ ''${#BRANCH} -eq 40 ]; then
+      REVISION=$BRANCH
+      NAME=$BRANCH
+  else
+      REVISION=$(${git}/bin/git ls-remote https://github.com/nixos/nixpkgs-channels $BRANCH | ${coreutils}/bin/cut -f1 )
+      NAME="$BRANCH.$DATE"
+  fi
+
+  TARBALL="https://github.com/NixOS/nixpkgs/archive/$REVISION.tar.gz"
+  SHA256=$(${nix}/bin/nix-prefetch-url --name $NAME --unpack $TARBALL)
+  COMMITTED=$(curl -s -X GET https://api.github.com/repos/NixOS/nixpkgs/git/commits/$REVISION | jq '.committer.date')
+
+  cat <<EOF
+  # nix-pinning $*
+  # committed on $COMMITTED - retrieved on $DATE
+  import (builtins.fetchTarball {
+    name   = "$NAME";
+    url    = "$TARBALL";
+    sha256 = "$SHA256";
+  })
+  EOF
+''
