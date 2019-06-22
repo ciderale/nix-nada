551b1660cb51257ba59497ce9de7d8a3b92e0123
Alain Lehmann <alain.lehmann@gmail.com>
2019-06-22 16:47:12 +0200

    override the package broken field selectively

--- default.nix
+++ default.nix
@@ -5,13 +5,21 @@ let
         # testsuite has an issue on osx
         # https://github.com/alanz/ghc-exactprint/issues/66
         ghc-exactprint = self.haskell.lib.dontCheck hsuper.ghc-exactprint;
-        multistate = self.haskell.lib.dontCheck hsuper.multistate;
+        multistate = self.haskell.lib.dontCheck (
+          self.haskell.lib.overrideCabal hsuper.multistate (old: {
+            broken=false;
+        }));
+
+        butcher = self.haskell.lib.overrideCabal hsuper.butcher (old: {
+          broken=false;
+        });
 
         brittany = self.haskell.lib.overrideCabal (
           self.haskell.lib.doJailbreak hsuper.brittany) (old: {
   version = "0.12.0.0";
   revision=null;
   editedCabalFile=null;
+  broken=false;
   sha256 = "058ffj00x374iaz75zzd9l0ab4crdsq2zjrq1r8lvcpp63fmsa7h";
           });
 
