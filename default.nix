ff090fe4ba1f34e5940af3288e80008bcc643284
Alain Lehmann <alain.lehmann@gmail.com>
2019-06-23 22:56:44 +0200

    moving packages into separate overlay

--- default.nix
+++ default.nix
@@ -1,27 +1,26 @@
 let
   overlay = import ./haskellOverlay.nix;
+  overlay2 = self: super: {
+    nix-pinning = self.callPackage ./packages/nix-pinning.nix {};
+    nix-update = self.callPackage ./packages/nix-update.nix {};
+    lorri = self.callPackage ./packages/lorri.nix { };
+    myzsh = self.callPackage ./packages/zsh.nix {};
+    myvim = self.callPackage ./packages/vim { git = self.gitFull; };
+  };
   pkgs = import ./nix { version="1903-darwin"; } {
     config = {};
-    overlays = [overlay];
+    overlays = [overlay overlay2];
   };
 in
 
 with pkgs;
 
-let
-  git = gitFull;
-  nix-pinning = callPackage ./packages/nix-pinning.nix {};
-  nix-update = callPackage ./packages/nix-update.nix {};
-  zsh = callPackage ./packages/zsh.nix {};
-  vim = callPackage ./packages/vim { git = gitFull; };
-  lorri = callPackage ./packages/lorri.nix { };
-in
 # install globally with
 # nix-env -f ./default.nix -iA buildInputs
 mkShell {
   buildInputs = [
     nix-pinning nix-update
-    zsh vim git
+    myzsh myvim gitFull
     which coreutils less
     curl jq
     direnv lorri.lorri
