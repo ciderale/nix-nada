9722009b0c7228e834550ee9d6482762099895f6
Alain Lehmann <alain.lehmann@gmail.com>
2019-06-23 21:02:04 +0200

    remove old versions file

--- nix/1809-darwin.nix
+++
@@ -1,7 +0,0 @@
-# nix-pinning nixpkgs-18.09-darwin
-# committed on "2019-04-08T03:04:42Z" - retrieved on 2019-04-11
-import (builtins.fetchTarball {
-  name   = "nixpkgs-18.09-darwin.2019-04-11";
-  url    = "https://github.com/NixOS/nixpkgs/archive/feaf8ac4632c2d9d27f24272da2e6873d2e9a7ad.tar.gz";
-  sha256 = "0qz8qmvh9vb6n1ziflhlww0pqqw936228pcpwnsyl8adi2izc7lw";
-})
