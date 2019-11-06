6114cf1e74ee8b55076ed9a8e8f9e813495e83a5
Alain Lehmann <alain.lehmann@gmail.com>
2019-11-06 21:49:42 +0100

    remove old nix-update script

--- packages/overlay.nix
+++ packages/overlay.nix
@@ -1,6 +1,5 @@
 self: super: {
   nix-pinning = self.callPackage ./nix-pinning.nix {};
-  nix-update = self.callPackage ./nix-update.nix {};
   lorri = self.callPackage ./lorri.nix { };
   myzsh = self.callPackage ./zsh.nix {};
   myvim = self.callPackage ./vim { git = self.gitFull; };
